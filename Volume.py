1394c5dc192cd7cc2918b2f084fa38625cba7be5
karanvijaygit <karanvijay20@gmail.com>
2018-12-15 15:28:39 +0530

    Add files via upload

---
+++ Volume.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 15 15:10:42 2018
+
+@author: karan
+"""
+
+# Find volume of a sphere with dia 12 cm 
+dia = 12
+Vol = (4/3)*(22/7)*dia*dia*dia
+print(Vol)
---
+++ input_user_name.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 15 15:06:53 2018
+
+@author: karan
+"""
+# Take user's first name and last name and print them in reverse order with space
+fn = input("Enter your first name:")
+ln = input("Enter your last name:")
+print(ln,fn,sep=" ")
