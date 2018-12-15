7daf7b5a2ff3feaff8f32a987ffbe65bad4441d7
karanvijaygit <karanvijay20@gmail.com>
2018-12-15 15:03:12 +0530

    Add files via upload

---
+++ list_print.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 15 13:49:49 2018
+
+@author: karan
+"""
+# Program to print numbers between 2000 and 3200(both included)divisible by 7
+# and not by 5 in a single line separated by comma
+output = []
+list1 = [1,2,3,4,5,6]
+ctr=0
+for i in range(2000,3201):
+    if i %7 ==0 and i%5!=0:
+        output.append(i)
+        ctr+=1
+print(*output,sep=" , ")
+print(ctr)
